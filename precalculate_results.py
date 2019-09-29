9c5c05886121d641dad885f4489d3f95dc632c6f
Tom Brown <tom@nworbmot.org>
2019-09-29 20:18:18 +0200

    Integrate new DEA assumptions to GUI; recalcalculate basic results

--- precalculate_results.py
+++ precalculate_results.py
@@ -4,40 +4,40 @@ import hashlib
 
 import pandas as pd
 
-location_type = "region"
+countries = "country:" + pd.Index(solve.get_country_multipolygons().keys())
 
-multipolygons = getattr(solve,"get_{}_multipolygons".format(location_type))()
+regions = "region:" + pd.Index(solve.get_region_multipolygons().keys())
+
+locations = countries.append(regions)
 
 years = range(2011,2014)
 
 techs = ["solar","onwind"]
 
-cf_exponents = [0.,1.,2.]
+cf_exponents = [2.,1.,0.]
 
 assumption_years = [2020,2030,2050]
-
-
-
-booleans = ["wind","solar","battery","hydrogen"]
-
-floats = ["cf_exponent","load","wind_cost","solar_cost","battery_energy_cost",
-          "battery_power_cost","hydrogen_electrolyser_cost",
-          "hydrogen_energy_cost",
-          "hydrogen_electrolyser_efficiency",
-          "hydrogen_turbine_cost",
-          "hydrogen_turbine_efficiency",
-          "discount_rate"]
-
-ints = ["year","frequency"]
-
 
 base_assumptions = {"wind" : True,
                     "solar" : True,
                     "battery" : True,
                     "hydrogen" : True,
+                    "dispatchable1": False,
+                    "dispatchable2": False,
+                    "co2_limit": False,
+                    "dispatchable1_cost" : 400,
+      	            "dispatchable1_marginal_cost" : 50,
+       	            "dispatchable1_emissions" : 500,
+       	            "dispatchable1_discount" : 10,
+       	            "dispatchable2_cost" : 6000,
+       	            "dispatchable2_marginal_cost" : 10,
+       	            "dispatchable2_emissions" : 0,
+       	            "dispatchable2_discount" : 10,
+                    "co2_emissions" : 0,
                     "load" : 100.0,
                     "frequency" : 3,
-                    "discount_rate" : 5.0}
+                    "discount_rate" : 5.0,
+                    "version" : solve.current_version}
 
 
 #copied from static/solver.js
@@ -73,26 +73,39 @@ tech_assumptions = {"2020" : {"wind_cost" : 1120,
 		    }
 }
 
-for year in years:
-    for cf_exponent in cf_exponents:
-        for location in multipolygons:
-            weather_hex = hashlib.md5("{}&{}&{}".format((location_type + ":" + location).replace("country:",""), year, cf_exponent).encode()).hexdigest()
+for cf_exponent in cf_exponents:
+    for year in years:
+        for location in locations:
+            print(year,cf_exponent,location)
+            weather_hex = hashlib.md5("{}&{}&{}&{}".format(location.replace("country:",""), year, cf_exponent, base_assumptions["version"]).encode()).hexdigest()
             weather_csv = 'data/time-series-{}.csv'.format(weather_hex)
             pu = pd.read_csv(weather_csv,
                              index_col=0,
                              parse_dates=True)
+            snapshots = pd.date_range("{}-01-01".format(year),"{}-12-31 23:00".format(year),freq="H")
+            pu = pu.reindex(snapshots,method="nearest")
+
             for assumption_year in assumption_years:
                 assumptions = base_assumptions.copy()
                 assumptions.update(tech_assumptions[str(assumption_year)])
                 assumptions["year"] = year
                 assumptions["cf_exponent"] = cf_exponent
-                assumptions["location"] = location_type + ":" + location
+                assumptions["location"] = location
                 results_string = assumptions["location"].replace("country:","")
 
-                for item in ints+booleans:
+                for item in solve.floats:
+                    assumptions[item] = float(assumptions[item])
+
+                for item in solve.ints+solve.booleans+solve.floats:
+                    if "dispatchable1" in item and not assumptions["dispatchable1"]:
+                        continue
+                    if "dispatchable2" in item and not assumptions["dispatchable2"]:
+                        continue
+                    if "co2" in item and not assumptions["co2_limit"]:
+                        continue
+                    if item == "version" and assumptions["version"] == 0:
+                        continue
                     results_string += "&{}".format(assumptions[item])
-                for item in floats:
-                    results_string += "&{}".format(float(assumptions[item]))
 
                 assumptions['results_hex'] = hashlib.md5(results_string.encode()).hexdigest()
                 print(results_string)
