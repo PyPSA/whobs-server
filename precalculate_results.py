fb19e6d54a1406113d9dd2a41f1aed7d37e00e18
Tom Brown <tom@nworbmot.org>
2023-08-17 15:32:36 +0200

    precalculate_results: update for new code

--- precalculate_results.py
+++ precalculate_results.py
@@ -2,7 +2,7 @@ import solve
 
 from server import sanitise_assumptions, compute_weather_hash, compute_results_hash
 
-import hashlib, json
+import hashlib, json, yaml
 
 import pandas as pd
 
@@ -14,159 +14,30 @@ locations = countries.append(regions)
 
 country_names = solve.get_country_names()
 
-years = range(2011,2013)
+years = range(2011,2012)
 
 techs = ["solar","onwind"]
 
-cf_exponents = [2.,1.,0.]
+cf_exponents = [2.]
 
-assumption_years = [2020,2030,2050]
-
-
-#copied from static/solver.js
-tech_assumptions = {"2020" : {"wind_cost" : 1120,
-				  "wind_fom" : 3,
-				  "wind_lifetime" : 25,
-				  "solar_cost" : 420,
-				  "solar_fom" : 3,
-				  "solar_lifetime" : 25,
-				  "battery_energy_cost" : 232,
-				  "battery_energy_fom" : 3,
-				  "battery_energy_lifetime" : 15,
-				  "battery_power_cost" : 270,
-				  "battery_power_fom" : 3,
-				  "battery_power_lifetime" : 15,
-				  "battery_power_efficiency_charging" : 95,
-				  "battery_power_efficiency_discharging" : 95,
-				  "hydrogen_energy_cost" : 0.7,
-				  "hydrogen_energy_fom" : 14,
-				  "hydrogen_energy_lifetime" : 25,
-				  "hydrogen_electrolyser_cost" : 1100,
-				  "hydrogen_electrolyser_efficiency" : 58,
-				  "hydrogen_electrolyser_fom" : 3,
-				  "hydrogen_electrolyser_lifetime" : 20,
-				  "hydrogen_turbine_cost" : 880,
-				  "hydrogen_turbine_efficiency" : 56,
-				  "hydrogen_turbine_fom" : 3,
-				  "hydrogen_turbine_lifetime" : 25,
-				  "dispatchable1_cost" : 400,
-				  "dispatchable1_marginal_cost" : 50,
-				  "dispatchable1_emissions" : 500,
-				  "dispatchable1_fom" : 3,
-				  "dispatchable1_lifetime" : 25,
-				  "dispatchable2_cost" : 6000,
-				  "dispatchable2_marginal_cost" : 10,
-				  "dispatchable2_emissions" : 0,
-				  "dispatchable2_fom" : 3,
-				  "dispatchable2_lifetime" : 25,
-				 },
-			"2030" : {"wind_cost" : 1040,
-				  "wind_fom" : 3,
-				  "wind_lifetime" : 25,
-				  "solar_cost" : 300,
-				  "solar_fom" : 3,
-				  "solar_lifetime" : 25,
-				  "battery_energy_cost" : 142,
-				  "battery_energy_fom" : 3,
-				  "battery_energy_lifetime" : 15,
-				  "battery_power_cost" : 160,
-				  "battery_power_fom" : 3,
-				  "battery_power_lifetime" : 15,
-				  "battery_power_efficiency_charging" : 95,
-				  "battery_power_efficiency_discharging" : 95,
-				  "hydrogen_energy_cost" : 0.7,
-				  "hydrogen_energy_fom" : 14,
-				  "hydrogen_energy_lifetime" : 25,
-				  "hydrogen_electrolyser_cost" : 600,
-				  "hydrogen_electrolyser_efficiency" : 62,
-				  "hydrogen_electrolyser_fom" : 3,
-				  "hydrogen_electrolyser_lifetime" : 20,
-				  "hydrogen_turbine_cost" : 830,
-				  "hydrogen_turbine_efficiency" : 58,
-				  "hydrogen_turbine_fom" : 3,
-				  "hydrogen_turbine_lifetime" : 25,
-				  "dispatchable1_cost" : 400,
-				  "dispatchable1_marginal_cost" : 50,
-				  "dispatchable1_emissions" : 500,
-				  "dispatchable1_fom" : 3,
-				  "dispatchable1_lifetime" : 25,
-				  "dispatchable2_cost" : 6000,
-				  "dispatchable2_marginal_cost" : 10,
-				  "dispatchable2_emissions" : 0,
-				  "dispatchable2_fom" : 3,
-				  "dispatchable2_lifetime" : 25,
-				 },
-			"2050" : {"wind_cost" : 960,
-				  "wind_fom" : 3,
-				  "wind_lifetime" : 25,
-				  "solar_cost" : 240,
-				  "solar_fom" : 3,
-				  "solar_lifetime" : 25,
-				  "battery_energy_cost" : 75,
-				  "battery_energy_fom" : 3,
-				  "battery_energy_lifetime" : 15,
-				  "battery_power_cost" : 60,
-				  "battery_power_fom" : 3,
-				  "battery_power_lifetime" : 15,
-				  "battery_power_efficiency_charging" : 95,
-				  "battery_power_efficiency_discharging" : 95,
-				  "hydrogen_energy_cost" : 0.7,
-				  "hydrogen_energy_fom" : 14,
-				  "hydrogen_energy_lifetime" : 25,
-				  "hydrogen_electrolyser_cost" : 400,
-				  "hydrogen_electrolyser_efficiency" : 67,
-				  "hydrogen_electrolyser_fom" : 3,
-				  "hydrogen_electrolyser_lifetime" : 20,
-				  "hydrogen_turbine_cost" : 800,
-				  "hydrogen_turbine_efficiency" : 60,
-				  "hydrogen_turbine_fom" : 3,
-				  "hydrogen_turbine_lifetime" : 25,
-				  "dispatchable1_cost" : 400,
-				  "dispatchable1_marginal_cost" : 50,
-				  "dispatchable1_emissions" : 500,
-				  "dispatchable1_fom" : 3,
-				  "dispatchable1_lifetime" : 25,
-				  "dispatchable2_cost" : 6000,
-				  "dispatchable2_marginal_cost" : 10,
-				  "dispatchable2_emissions" : 0,
-				  "dispatchable2_fom" : 3,
-				  "dispatchable2_lifetime" : 25,
-				 }
-		       };
-
-defaults = {"version" : 190929,
-	    "location" : "country:DE",
-	    "location_name" : "Germany",
-	    "job_type" : "none",
-	    "year" : 2011,
-	    "frequency" : 3,
-	    "cf_exponent" : 2,
-	    "load" : 100,
-	    "hydrogen_load" : 0,
-	    "wind_min" : 0,
-	    "solar_min" : 0,
-	    "wind_max" : 1e7,
-	    "solar_max" : 1e7,
-	    "wind_discount" : 5,
-	    "solar_discount" : 5,
-	    "battery_energy_discount" : 5,
-	    "battery_power_discount" : 5,
-	    "hydrogen_energy_discount" : 5,
-	    "hydrogen_electrolyser_discount" : 5,
-	    "hydrogen_turbine_discount" : 5,
-	    "dispatchable1_discount" : 10,
-	    "dispatchable2_discount" : 10,
-	    "co2_emissions" : 100,
-	    "wind" : True,
-	    "solar" : True,
-	    "battery" : True,
-	    "hydrogen" : True,
-	    "dispatchable1" : False,
-	    "dispatchable2" : False,
-	    "co2_limit" : False,
-	   };
+assumption_years = [2030]
 
 
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+
+#na_filter leaves "" as "" rather than doing nan which confuses jinja2
+defaults = pd.read_csv("defaults.csv",index_col=[0,1],na_filter=False)
+
+for (n,t) in [("f",float),("i",int)]:
+    defaults.loc[defaults["type"] == n, "value"] = defaults.loc[defaults["type"] == n,"value"].astype(t)
+
+#work around fact bool("False") returns True
+defaults.loc[defaults.type == "b","value"] = (defaults.loc[defaults.type == "b","value"] == "True")
+
+defaults_t = {str(year): defaults.swaplevel().loc[str(year)]["value"].to_dict() for year in config["tech_years"]}
+defaults = defaults.swaplevel().loc[""]["value"].to_dict()
 
 defaults["job_type"] = "solve"
 
@@ -194,7 +65,7 @@ for cf_exponent in cf_exponents:
 
             for assumption_year in assumption_years:
                 assumptions = assumptions_base.copy()
-                assumptions.update(tech_assumptions[str(assumption_year)])
+                assumptions.update(defaults_t[str(assumption_year)])
 
                 error_message, assumptions = sanitise_assumptions(assumptions)
                 if error_message is not None:
@@ -203,15 +74,10 @@ for cf_exponent in cf_exponents:
 
                 assumptions['results_hex'] = compute_results_hash(assumptions)
                 print(assumptions['results_hex'])
-                series_csv = 'data/results-series-{}.csv'.format(assumptions['results_hex'])
-                overview_csv = 'data/results-overview-{}.csv'.format(assumptions['results_hex'])
 
-                print("Calculating results from scratch, saving as:", series_csv, overview_csv)
-                results_overview, results_series, error_msg = solve.run_optimisation(assumptions, pu)
+                print("Calculating results from scratch, saving with hex",assumptions['results_hex'])
+                error_msg = solve.run_optimisation(assumptions, pu)
                 print(error_msg)
-                results_series = results_series.round(1)
-                results_series.to_csv(series_csv)
-                results_overview.to_csv(overview_csv,header=False)
 
                 with open('data/results-assumptions-{}.json'.format(assumptions['results_hex']), 'w') as fp:
                     json.dump(assumptions,fp)
